35f7a25267437c747dec6b7b73dc18459c462543
gladja <gladkih.alexander@gmail.com>
2024-03-24 21:00:44 +0200

    add faq

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,10 +1,70 @@
 import { Section } from '../Section';
-import { Title } from '../Title';
+import { Title } from '../Title/';
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
+import {
+  TitleWrap,
+  Text,
+  AccordionItemCustom,
+  AccordionCustom,
+  AccordionItemButtonCustom,
+  AccordionItemPanelCustom,
+  WrapAccord,
+  TitleBlock,
+  Number,
+} from './Faq.styled';
 
 export const Faq = () => {
   return (
     <Section>
       <Title text="Faq" />
+      <WrapAccord>
+        <AccordionCustom>
+          <AccordionItemCustom>
+            <AccordionItemHeading>
+              <AccordionItemButtonCustom>
+                <TitleBlock>
+                  <Number>[&nbsp;1&nbsp;]</Number>
+                  <TitleWrap>WHAT IS AN NFT COLLECTION?</TitleWrap>
+                </TitleBlock>
+              </AccordionItemButtonCustom>
+            </AccordionItemHeading>
+            <AccordionItemPanelCustom>
+              <Text>
+                An NFT collection is a group of unique digital assets, each
+                represented by a non-fungible token, typically created around a
+                specific theme or style.
+              </Text>
+            </AccordionItemPanelCustom>
+          </AccordionItemCustom>
+
+          <AccordionItemCustom>
+            <AccordionItemHeading>
+              <AccordionItemButtonCustom>
+                <TitleBlock>
+                  <Number>[&nbsp;2&nbsp;]</Number>
+                  <TitleWrap>
+                    HOW DO I PURCHASE NFTS FROM A COLLECTION?
+                  </TitleWrap>
+                </TitleBlock>
+              </AccordionItemButtonCustom>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <Text>
+                Launch your own token, the proceeds of which will go to a global
+                fund to LAUNCH YACHT CLUB AND PROMOTE it
+              </Text>
+            </AccordionItemPanel>
+          </AccordionItemCustom>
+        </AccordionCustom>
+      </WrapAccord>
     </Section>
   );
 };
--- src/components/Faq/Faq.styled.js
+++ src/components/Faq/Faq.styled.js
@@ -1,1 +1,85 @@
 import styled from 'styled-components';
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+export const WrapAccord = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const AccordionCustom = styled(Accordion)``;
+
+export const AccordionItemCustom = styled(AccordionItem)`
+  width: 216px;
+  padding: 8px;
+  border-radius: 12px;
+
+  &:focus-within,
+  &:visited {
+    background: ${({ theme }) => theme.colors.black2};
+  }
+`;
+
+export const AccordionItemButtonCustom = styled(AccordionItemButton)`
+  /* .accordion__button { */
+  /* background: ${({ theme }) => theme.colors.black2}; */
+
+  /* &:hover {
+    color: red;
+  } */
+  /* } */
+`;
+
+export const TitleBlock = styled.div`
+  display: flex;
+  gap: 8px;
+
+  transition: color 0.3s;
+
+  &:focus-within,
+  &:focus,
+  &:hover,
+  &:active {
+    color: ${({ theme }) => theme.colors.red};
+  }
+
+  &:hover > p {
+    color: ${({ theme }) => theme.colors.white};
+  },
+`;
+
+export const Number = styled.p`
+  font-family: BiroScriptPlus, sans-serif;
+  font-size: 12px;
+  margin-top: 8px;
+  color: ${({ theme }) => theme.colors.red};
+`;
+
+export const AccordionItemPanelCustom = styled(AccordionItemPanel)`
+  padding: 10px 8px 10px 30px;
+`;
+
+export const TitleWrap = styled.h3`
+  font-family: RightGrotesk, sans-serif;
+  font-size: 20px;
+  text-transform: uppercase;
+
+  &:focus,
+  &:hover,
+  &:active {
+    color: ${({ theme }) => theme.colors.red};
+  }
+`;
+
+export const Text = styled.p`
+  font-family: MessinaSansMono, sans-serif;
+  font-size: 12px;
+  text-transform: uppercase;
+  line-height: calc(14 / 12);
+`;
